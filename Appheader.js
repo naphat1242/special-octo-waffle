71bb578730d955d557d73a95fb79575a30a3c263
naphat1242 <76762726+naphat1242@users.noreply.github.com>
2021-02-17 12:27:45 +0700

    Add files via upload

---
+++ Appheader.js
@@ -0,0 +1,12 @@
+function Appheader() {
+
+return (
+
+      <header className="App">
+        <img className = "header-logo" src = "/pic/shiba.jpg"/>
+       
+
+      </header>
+        );
+      }
+export default Appheader;
---
+++ items.js
@@ -0,0 +1,28 @@
+const items =[
+    {
+      title :"man",
+      thumbnaiUrl :"/pic/man.jpg"
+      
+    
+    },
+    {
+      title :"girl",
+      thumbnaiUrl :"/pic/women.jpg"
+      
+    
+    },
+    {
+      title :"boy",
+      thumbnaiUrl :"/pic/son.jpg"
+      
+    
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/pic/shiba.jpg"
+      
+    
+    }
+  ];
+  export default items;
+
